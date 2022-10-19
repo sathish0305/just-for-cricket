419be22e7e016d02311aaa3bc25f67f57929de4b
Sathish <sathishms0305@gmail.com>
2022-10-19 18:25:46 +0530

    fab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from './components/Navbar/Navigation'
 import Hero from "./hero/Hero";
 import Footer from './components/footer/Footer'
+import Fab from "./components/Fab/Fab";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   <Navigation/>
   <Hero/>
   <Footer/>
+  <Fab/>
   </>
   );
 }
---
+++ src/components/Fab/Fab.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './fab.css'
+function Fab() {
+  return (
+   <>
+   <a href="http://api.whatsapp.com/send?phone=919902603554" class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+   </>
+  );
+}
+
+export default Fab
