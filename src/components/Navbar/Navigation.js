633f04a9db8de99d3b102408755d7bde850e63a7
Sathish <sathishms0305@gmail.com>
2022-10-19 14:19:27 +0530

    commit

---
+++ src/components/Navbar/Navigation.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './navigation.css'
+
+
+function Navigation() {
+  return (
+  <>
+   <nav className="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+      <div className="container">
+        <a className="navbar-brand brand" href="/">Just for Cricket</a>
+        <button
+          className="navbar-toggle"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggle-icon"></span>
+        </button>
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="mx-auto"></div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </>
+);
+}
+
+export default Navigation;
